3bfa9073431e270d23fda630b7aabcd9b8244e5a
JayvonK <jaykhuth@gmail.com>
2024-01-25 01:16:12 -0800

    madlib

---
+++ Services/MadLib/IMadLibService.cs
@@ -0,0 +1,6 @@
+namespace HostingAllForOne.Services.MadLib;
+
+    public interface IMadLibService
+    {
+        string MadLib(string celebrityName, string animal, string bodyPart, string nounThatEndsWithS, string wholeNumberGreaterThan1, string time, string height, string adjective, string name, string condiment);
+    }
