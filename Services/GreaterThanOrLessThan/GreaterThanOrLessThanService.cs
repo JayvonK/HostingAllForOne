67066b7ab06be3797a69078e14fc8f888d3bcd76
JayvonK <jaykhuth@gmail.com>
2024-01-25 01:10:22 -0800

    greater than or less than

--- Services/GreaterThanOrLessThan/GreaterThanOrLessThanService.cs
+++ Services/GreaterThanOrLessThan/GreaterThanOrLessThanService.cs
@@ -1,12 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace HostingAllForOne.Services.GreaterThanOrLessThan;
 
-namespace HostingAllForOne.Services.GreaterThanOrLessThan
+public class GreaterThanOrLessThanService : IGreaterThanOrLessThanService
 {
-    public class GreaterThanOrLessThanService
+    public string GOrL(string numOne, string numTwo)
     {
-        
+        if (double.TryParse(numOne, out double num) && double.TryParse(numTwo, out double num2))
+        {
+            if (num > num2)
+            {
+                return $"The first number: {numOne}, is greater than the second number: {numTwo}. \nThe second number: {numTwo}, is less than the first number: {numOne}.";
+            }
+            else if (num < num2)
+            {
+                return $"The first number: {numOne}, is less than the second number: {numTwo}. \nThe second number: {numTwo}, is greater than the first number: {numOne}.";
+            }
+            else
+            {
+                return $"The first number: {numOne}, is equal to the second number: {numTwo}. \nThe second number: {numTwo}, is equal to the first number: {numOne}.";
+            }
+        }
+        else
+        {
+            return "Error, you need to enter in numbers for both parameters.";
+        }
     }
 }
--- Services/GreaterThanOrLessThan/IGreaterThanOrLessThanService.cs
+++ Services/GreaterThanOrLessThan/IGreaterThanOrLessThanService.cs
@@ -2,5 +2,5 @@ namespace HostingAllForOne.Services.GreaterThanOrLessThan;
 
     public interface IGreaterThanOrLessThanService
     {
-        
+        string GOrL(string numOne, string numTwo);
     }
