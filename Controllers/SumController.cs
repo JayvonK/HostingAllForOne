8b96db8479ff271e742257c61f9aed91388435af
JayvonK <jaykhuth@gmail.com>
2024-01-25 02:11:57 -0800

    sum controller

--- Controllers/SumController.cs
+++ Controllers/SumController.cs
@@ -1,11 +1,24 @@
+using HostingAllForOne.Services.Sum;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HostingAllForOne.Controllers;
 
-    [ApiController]
-    [Route("[controller]")]
+[ApiController]
+[Route("[controller]")]
 
-    public class SumController : ControllerBase
+public class SumController : ControllerBase
+{
+    private readonly ISumService _sumService;
+
+    public SumController(ISumService sumService)
     {
-        
+        _sumService = sumService;
     }
+
+    [HttpGet]
+    [Route("EnterTwoNumbers/{firstNumber}/{secondNumber}")]
+    public string AddTwoNumbers(string firstNumber, string secondNumber)
+    {
+        return _sumService.Sum(firstNumber, secondNumber);
+    }
+}
