d139766f0314d312d8aa268660f7716f0a8dec7d
JayvonK <jaykhuth@gmail.com>
2024-01-25 02:00:37 -0800

    builder services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
+using HostingAllForOne.Services.GreaterThanOrLessThan;
+using HostingAllForOne.Services.MadLib;
+using HostingAllForOne.Services.NameTime;
+using HostingAllForOne.Services.OddOrEven;
+using HostingAllForOne.Services.ReverseItAlphanumeric;
+using HostingAllForOne.Services.ReverseItNumbers;
+using HostingAllForOne.Services.SayHello;
+using HostingAllForOne.Services.Sum;
+using MiniChallengeEightToTen_Endpoints.Services.EightBall;
+using MiniChallengeEightToTen_Endpoints.Services.RestaurantPicker;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +17,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEightBallService, EightBallService>();
+builder.Services.AddScoped<IGreaterThanOrLessThanService, GreaterThanOrLessThanService>();
+builder.Services.AddScoped<IMadLibService, MadLibService>();
+builder.Services.AddScoped<INameTimeService, NameTimeService>();
+builder.Services.AddScoped<IOddOrEvenService, OddOrEvenService>();
+builder.Services.AddScoped<IRestaurantPickerService, RestaurantPickerService>();
+builder.Services.AddScoped<IReverseItAlphanumericService, ReverseItAlphanumeric>();
+builder.Services.AddScoped<IReverseItNumbersService, ReverseItNumbersService>();
+builder.Services.AddScoped<ISayHelloService, SayHellosService>();
+builder.Services.AddScoped<ISumService, SumService>();
 
 var app = builder.Build();
 
